f7728bb9ff3128dc3aa026f1651f19134e874564
Kingsley1234 <Nonnystar53@gmail.com>
2020-02-02 21:03:51 -0400

    implemented the search algo

--- Im_Hungry/src/pages/search.vue
+++ Im_Hungry/src/pages/search.vue
@@ -1,13 +1,15 @@
 <template>
   <f7-page name="search">
-  <!-- <f7-appbar> -->
     <f7-searchbar
         custom-search
+        :disable-button="!$theme.aurora"
+         placeholder="Feed thy self"
+         @input="keyword = $event.target.value"
+         @keyup.native.enter="search"
       ></f7-searchbar>
-  <!-- </f7-appbar> -->
     <!-- Top Navbar -->
     <span v-for="(items, index) in restaurant" :key="index">
-      <RestaurantCard :title="items.title"/>
+      <RestaurantCard :title="items.name"/>
     </span>
   </f7-page>
 </template>
@@ -20,24 +22,21 @@ export default {
   },
   data(){
     return{
-      restaurant: [
-                {
-                  id: '1',
-                  title: 'Joes',
-                  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
-                },
-                {
-                  id: 'Piccaso',
-                  title: 'Picasso',
-                  description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
-                },
-                {
-                  id: 'Prontos',
-                  title: 'Prontos',
-                  description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
-                },
-              ]
+      keyword: ''
     }
+  },
+  computed: {
+    restaurant(){
+      return this.$store.state.restaurant
+    }
+  },
+  methods: {
+    search(){
+      this.$store.dispatch('getRestaurant', this.keyword)
+    }
+
+  },
+  created(){
   }
 };
 </script>
---
+++ Im_Hungry/src/store/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        restaurant: [
+        ]
+    },
+    mutations: {
+        GET_RESTAURANT(state, keyword) {
+            var key = 'AIzaSyCcZ-5hQ41xZ3SyZYBeZWERrGVHKgvkzS0'
+            var location = '45.08333,-64.36667'
+            var radius = 16000;
+            var sensor = false;
+            var types = "restaurant";
+            var url2 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types + "&keyword=" + keyword;
+
+            axios.get(url2).then(res => {
+                console.log(res.data)
+                state.restaurant = res.data.results
+            })
+
+        }
+    },
+    actions: {
+        getRestaurant({ commit }, keyword) {
+            commit('GET_RESTAURANT', keyword)
+        }
+    },
+})  
